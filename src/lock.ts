021e1ccce86dcf068face0f3e28311e8e430ad80
Thibault Le Ouay <thibaultleouay@gmail.com>
2024-12-24 09:02:31 +0100

    implement locking

--- src/lock.ts
+++ src/lock.ts
@@ -2,27 +2,46 @@ import { DurableObject } from "cloudflare:workers";
 import type { Bindings } from ".";
 
 export class Lock extends DurableObject {
-      locked_at: string | null
-      state: DurableObjectState
+	locked_at: string | null;
+	path: string | null;
+	state: DurableObjectState;
 
-      constructor(state: DurableObjectState,  env: Bindings) {
-        super(state, env)
-        this.locked_at = null
-        this.state = state
-      }
+	constructor(state: DurableObjectState, env: Bindings) {
+		super(state, env);
+		this.state = state;
+		this.locked_at = null;
+		this.path = null;
 
-    async lock(){
-        this.locked_at = new Date().toISOString()
-        this.state.storage.put("locked_at", this.locked_at)
-        return this.locked_at
-    }
+		this.state.blockConcurrencyWhile(async () => {
+			const locked_at = await this.state.storage.get<string>("locked_at");
+			const path = await this.state.storage.get<string>("path");
+			if (locked_at) {
+				this.locked_at = locked_at;
+			}
+			if (path) {
+				this.path = path;
+			}
+		});
+	}
 
-    async unlock(){
-        this.locked_at = null
-        this.state.storage.delete("locked_at")
-    }
+	async lock({ path }: { path: string }) {
+		this.locked_at = new Date().toISOString();
+		await this.state.storage.put("locked_at", this.locked_at);
+		this.path = path;
+		await this.state.storage.put("path", this.path);
+
+		console.log("locked", this.locked_at, this.path);
+		return this.locked_at;
+	}
+
+	async unlock() {
+		this.locked_at = null;
+		this.path = null;
+		await this.state.storage.delete("path");
+		await this.state.storage.delete("locked_at");
+	}
+
+	async get() {
+		return { locked_at: this.locked_at, path: this.path };
+	}
 }
-
-
-
-
--- src/schema.ts
+++ src/schema.ts
@@ -1,106 +1,108 @@
 import { z } from "zod";
 
 export const batchRequestSchema = z.object({
-  operation: z.enum(["upload", "download"]),
-  ref: z.object({ name: z.string() }).optional(),
-  transfers: z.array(z.string()),
-  objects: z.array(z.object({
-    oid: z.string(),
-    size: z.number().gte(0),
-    authenticated: z.boolean().optional(),
-  })),
-  hash_algo: z.string().default("sha256"),
+	operation: z.enum(["upload", "download"]),
+	ref: z.object({ name: z.string() }).optional(),
+	transfers: z.array(z.string()),
+	objects: z.array(
+		z.object({
+			oid: z.string(),
+			size: z.number().gte(0),
+			authenticated: z.boolean().optional(),
+		}),
+	),
+	hash_algo: z.string().default("sha256"),
 });
 
 export const batchResponseSchema = z.object({
-  transfer: z.string().optional(),
-  objects: z.array(
-    z
-      .object({
-        oid: z.string(),
-        size: z.number().gte(0),
-        authenticated: z.boolean().optional(),
-        actions: z
-          .object({
-            download: z
-              .object({
-                href: z.string(),
-                header: z.record(z.any()).optional(),
-                expires_in: z
-                  .number()
-                  .gte(-2147483647)
-                  .lte(2147483647)
-                  .optional(),
-                expires_at: z.string().optional(),
-              })
-              .strict()
-              .optional(),
-            upload: z
-              .object({
-                href: z.string(),
-                header: z.record(z.any()).optional(),
-                expires_in: z
-                  .number()
-                  .gte(-2147483647)
-                  .lte(2147483647)
-                  .optional(),
-                expires_at: z.string().optional(),
-              })
-              .strict()
-              .optional(),
-            verify: z
-              .object({
-                href: z.string(),
-                header: z.record(z.any()).optional(),
-                expires_in: z
-                  .number()
-                  .gte(-2147483647)
-                  .lte(2147483647)
-                  .optional(),
-                expires_at: z.string().optional(),
-              })
-              .strict()
-              .optional(),
-          })
-          .strict()
-          .optional(),
-        error: z
-          .object({ code: z.number(), message: z.string() })
-          .strict()
-          .optional(),
-      })
-      .strict(),
-  ),
-  message: z.string().optional(),
-  request_id: z.string().optional(),
-  documentation_url: z.string().optional(),
+	transfer: z.string().optional(),
+	objects: z.array(
+		z
+			.object({
+				oid: z.string(),
+				size: z.number().gte(0),
+				authenticated: z.boolean().optional(),
+				actions: z
+					.object({
+						download: z
+							.object({
+								href: z.string(),
+								header: z.record(z.any()).optional(),
+								expires_in: z
+									.number()
+									.gte(-2147483647)
+									.lte(2147483647)
+									.optional(),
+								expires_at: z.string().optional(),
+							})
+							.strict()
+							.optional(),
+						upload: z
+							.object({
+								href: z.string(),
+								header: z.record(z.any()).optional(),
+								expires_in: z
+									.number()
+									.gte(-2147483647)
+									.lte(2147483647)
+									.optional(),
+								expires_at: z.string().optional(),
+							})
+							.strict()
+							.optional(),
+						verify: z
+							.object({
+								href: z.string(),
+								header: z.record(z.any()).optional(),
+								expires_in: z
+									.number()
+									.gte(-2147483647)
+									.lte(2147483647)
+									.optional(),
+								expires_at: z.string().optional(),
+							})
+							.strict()
+							.optional(),
+					})
+					.strict()
+					.optional(),
+				error: z
+					.object({ code: z.number(), message: z.string() })
+					.strict()
+					.optional(),
+			})
+			.strict(),
+	),
+	message: z.string().optional(),
+	request_id: z.string().optional(),
+	documentation_url: z.string().optional(),
 });
 
 export const createLockRequestSchema = z.object({
-  path: z.string(),
-  ref: z.object({ name: z.string() }).optional(),
+	path: z.string(),
+	ref: z.object({ name: z.string() }).optional(),
 });
 
 export const createLockResponseSchema = z.object({
-  lock: z.object({
-    id: z.string(),
-    path: z.string(),
-    locked_at: z.string(),
-    owner: z.object({ name: z.string().optional() }).optional(),
-  }),
-  message: z.string().optional(),
-  request_id: z.string().optional(),
-  documentation_url: z.string().optional(),
+	lock: z.object({
+		id: z.string(),
+		path: z.string(),
+		locked_at: z.string(),
+		owner: z.object({ name: z.string().optional() }).optional(),
+	}),
+	message: z.string().optional(),
+	request_id: z.string().optional(),
+	documentation_url: z.string().optional(),
 });
 
 export const lockListResponseSchema = z.object({
-  locks: z.array(
-    z.object({
-      id: z.string().optional(),
-      path: z.string().optional(),
-      locked_at: z.string().optional(),
-      owner: z.object({ name: z.string().optional() }).optional(),
-    }),
-  ),
-  next_cursor: z.string().optional(),
+	locks: z.array(
+		z.object({
+			id: z.string().optional(),
+			path: z.string().optional(),
+			locked_at: z.string().optional(),
+			owner: z.object({ name: z.string().optional() }).optional(),
+		}),
+	),
+	next_cursor: z.string().optional(),
 });
